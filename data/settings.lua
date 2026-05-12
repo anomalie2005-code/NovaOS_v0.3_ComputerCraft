76bf7e62107d88e675d6839f9cd7b4db150ae3ae
anomalie2005-code <anomalie2005@gmail.com>
2026-05-12 14:53:40 +0300

    Add files via upload

---
+++ data/settings.lua
@@ -0,0 +1,12 @@
+return {
+  shellName = "NovaShell",
+  promptStyle = "classic",
+  systemName = "NovaOS",
+  homeDir = "home/user",
+  username = "user",
+  version = "0.1.0",
+  hostname = "nova",
+  boxStyle = "ascii",
+  theme = "cyber",
+  showFetchOnBoot = true,
+}
